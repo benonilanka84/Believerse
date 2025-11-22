8c242921dce13d42fff60b0a6e63ad0d8188a628
Benoni Lanka <benonilanka84@gmail.com>
2025-11-22 23:02:57 +0530

    fix next config root layout path aliases

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "The Believerse",
+  description: "One Family in Christ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
